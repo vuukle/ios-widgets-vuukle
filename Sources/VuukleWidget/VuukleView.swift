efb266c18baaa09216ff68deca0a345559adb39e
NarekDallakyan <it.narek.dallakyan@gmail.com>
2023-09-28 15:53:16 +0400

    Event

--- Sources/VuukleWidget/VuukleView.swift
+++ Sources/VuukleWidget/VuukleView.swift
@@ -118,6 +118,9 @@ public class VuukleView: UIView, WebViewable {
 
 extension VuukleView: WKScriptMessageHandler {
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("WKScriptMessageHandler","\(message.body)")
+        
+        
         if let body = message.body as? String, body.contains(VuukleConstants.logoutClickedMessage.rawValue) {
             logoutEventListener?()
         }
