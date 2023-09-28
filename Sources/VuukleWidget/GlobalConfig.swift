f0ca9b96001e58a7717288854ff4cfd65cb687b3
NarekDallakyan <it.narek.dallakyan@gmail.com>
2023-09-28 16:05:04 +0400

    Event

--- Sources/VuukleWidget/GlobalConfig.swift
+++ Sources/VuukleWidget/GlobalConfig.swift
@@ -61,6 +61,6 @@ enum VuukleConstants: String {
     // MARK: - New
 
     case logoutClickedMessage = "logout-clicked"
-    case signInButtonClickedMessage = "SignInClickEvent"
+    case signInButtonClickedMessage = "signin-clicked"
 
 }
